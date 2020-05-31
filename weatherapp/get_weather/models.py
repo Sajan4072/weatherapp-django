87d4a2e2f6f8c90b962bed4b2095fb145e2f4fcd
Sajan4072 <rai4072@gmail.com>
2020-05-31 23:37:32 +0545

    model added for places

--- weatherapp/get_weather/models.py
+++ weatherapp/get_weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural='cities'
