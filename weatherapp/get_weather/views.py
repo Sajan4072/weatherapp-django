fc7391293e21eda2c3ffdb92b537226feb1c1931
Sajan4072 <rai4072@gmail.com>
2020-05-31 21:29:17 +0545

    requests package added

--- weatherapp/get_weather/views.py
+++ weatherapp/get_weather/views.py
@@ -1,5 +1,11 @@
+import requests
 from django.shortcuts import render
 
 
 def index(request):
+
+    url='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=41cf57a23a0785ed1c0f028ac06962d9'
+    city='Las Vegas'
+    res=requests.get(url.format(city))
+    print(res.text)
     return render (request,'landing.html')
