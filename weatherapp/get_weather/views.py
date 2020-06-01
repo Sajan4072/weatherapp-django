d08088b5e2eee9242ffa367461757a852d13e25e
Sajan4072 <rai4072@gmail.com>
2020-06-01 19:31:06 +0545

    multiple places added

--- weatherapp/get_weather/views.py
+++ weatherapp/get_weather/views.py
@@ -1,22 +1,36 @@
 import requests
 from django.shortcuts import render
-
+from .models import Place
 
 def index(request):
 
+
     url='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=41cf57a23a0785ed1c0f028ac06962d9'
     place='Las Vegas'
-    
-    res=requests.get(url.format(place)).json()
 
-    cweather={
-            'city' : place,
-            'temperature' : res['main']['temp'],
-            'description' : res['weather'][0]['description'],
-            'icon' : res['weather'][0]['icon'],
-    }
-    print(cweather)
 
-    context={'cweather':cweather}
+    cities=Place.objects.all()
+    w_data=[]
+
+    for place in cities:
+        res=requests.get(url.format(place)).json()
+
+        cweather={
+              'city' : place.name,
+              'temperature' : res['main']['temp'],
+               'description' : res['weather'][0]['description'],
+               'icon' : res['weather'][0]['icon'],
+        }
+        w_data.append(cweather)
     
-    return render (request,'landing.html',context)
+    print(w_data) 
+         
+    context={'w_data':w_data}
+    return render(request,'landing.html',context)  
+            
+
+    
+        
+     
+     
+   
